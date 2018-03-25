aa7da6cd98e6d6883c1a568d1593538583d6dea8
Dávid Szakállas <david.szakallas@gmail.com>
2018-03-25 19:10:05 +0200

    fix script

--- scripts/gh_upload.sh
+++ scripts/gh_upload.sh
@@ -9,12 +9,9 @@ cd $GITDIR
 git init
 git config user.name $GIT_USER
 git config user.email $GIT_EMAIL
-git remote add origin "https://github.com/${GH_REPO}.git"
-git pull
-git checkout gh-pages
 
-cp -r $OUTDIR/**/* $GITDIR
+cp -r $OUTDIR/**/* .
 
 git add .
-git commit -m $MESSAGE
-git push --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}.git" $BRANCH:$TGT_BRANCH  > /dev/null 2>&1
+git commit -m $COMMIT_MESSAGE
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}.git" $BRANCH:$TGT_BRANCH  > /dev/null 2>&1
